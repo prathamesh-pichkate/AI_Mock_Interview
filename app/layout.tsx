f3902c7e3f1f4925199cdab43d67402e9940381e
prathamesh-pichkate <prathameshpichkate@gmail.com>
2026-02-18 10:01:11 +0530

    Setup the vapi ai

--- app/layout.tsx
+++ app/layout.tsx
@@ -24,7 +24,7 @@ export default function RootLayout({
     <html lang="en" className="dark">
       <body className={`${monaSans.className} antialiased pattern`}>
         {children}
-        <Toaster />z
+        <Toaster />
       </body>
     </html>
   );
